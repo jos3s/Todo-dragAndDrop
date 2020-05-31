b1ea2d063c5e71765f38089eca925eeb16484ba0
jos3s <karatedkgt17@gmail.com>
2020-05-30 22:49:43 -0300

    Modificação/Adicionado a função de excluir notas e definir cor por tipo de nota criada

--- script.js
+++ script.js
@@ -1,31 +1,39 @@
-const cards = document.querySelectorAll('.card');
 const dropzones = document.querySelectorAll('.dropzone');
+const colors={
+    todo:'#42A5F5',
+    progress:'#43A047',
+    done:'#AB47BC'
 
-const form=document.querySelector('form');
-form.addEventListener('submit', e=>{
-    createElement();
-});
+}
 
-const colors=[]
-
+const cards=['Do videos!','Forum','Next Level Week'];
 
 document.addEventListener('click', el=>{
     e=el.target;
     if(e.classList.contains('delete')) deleteCard(e);
 });
 
-cards.forEach(card => {
-    card.addEventListener('dragstart', dragstart);
-    card.addEventListener('drag', drag);
-    card.addEventListener('dragend', dragend);
+document.addEventListener('submit', el=>{
+    el.preventDefault();
+    e=el.target;
+    if(e.querySelector('input').value !=' '){
+        createElement(e);
+        e.querySelector('input').value='';
+    }
 });
+
+createCardDrag();
+function createCardDrag() {
+    const cards = document.querySelectorAll('.card');
+    cards.forEach(card => {
+        card.addEventListener('dragstart', dragstart);
+        card.addEventListener('dragend', dragend);
+    });
+}
 
 function dragstart() {
     dropzones.forEach( dropzone => dropzone.classList.add('highlight'));
     this.classList.add('is-dragging');
-}
-
-function drag() {
 }
 
 function dragend() {
@@ -34,18 +42,16 @@ function dragend() {
 }
 
 dropzones.forEach( dropzone => {
-    dropzone.addEventListener('dragenter', dragenter);
     dropzone.addEventListener('dragover', dragover);
     dropzone.addEventListener('dragleave', dragleave);
     dropzone.addEventListener('drop', drop);
 });
 
-function dragenter() {
-}
-
 function dragover() {
     this.classList.add('over');
     const cardBeingDragged = document.querySelector('.is-dragging');
+    const status=cardBeingDragged.querySelector(".status");
+    status.style.background=colors[cardBeingDragged.parentElement.classList[1]];
     this.appendChild(cardBeingDragged);
 }
 
@@ -57,29 +63,49 @@ function drop() {
     this.classList.remove('over');
 }
 
-function deleteCard(e){
-    e.parentNode.remove()
+function createElement(form) {
+    const board=form.parentNode.parentNode.parentNode;
+    const dropzone=board.querySelector('.dropzone');
+    const input=form.querySelector('input').value;
+    if(dropzone.classList.contains("todo")) newCard=createCard(input,'todo');
+    if(dropzone.classList.contains("progress")) newCard=createCard(input,'progress');
+    if(dropzone.classList.contains("done")) newCard=createCard(input,'done');
+    dropzone.appendChild(newCard);
+    createCardDrag();
 }
 
-
-function createElement() {
-    const card=document.querySelector('.board-create');
-    const input=card.querySelector('input').value;
-    newCard=createCard(input)  
-    dropzones[0].appendChild(newCard);
-}
-
-function createCard(txt) {
+function createCard(txt,type) {
     const card=document.createElement('div');
     card.setAttribute('draggable','true');
     card.setAttribute('class','card');
     const status=document.createElement('div');
     status.setAttribute('class','status');
+    status.style.background=colors[type];
+    card.appendChild(status);
+    const content=createContent(txt);
+    card.appendChild(content);
+    const button=createButtonDelete();
+    card.appendChild(button);
+    return card;
+}
+
+function createContent(txt) {
     const content=document.createElement('div');
     content.setAttribute('class','content');
     const texto=document.createTextNode(txt)
     content.appendChild(texto);
-    card.appendChild(status);
-    card.appendChild(content);
-    return card;
+    return content
+}
+
+function createButtonDelete() {
+    const button=document.createElement('button');
+    button.setAttribute('class','delete');
+    button.setAttribute('title','Apagar');
+    const texto=document.createTextNode('Apagar')
+    button.appendChild(texto);
+    return button;
+}
+
+function deleteCard(e){
+    e.parentNode.remove()
 }
